75a200a8aa5ddd12a5bca0d9b6cfc2fa0da5189c
perceptr <st.arms.all@gmail.com>
2022-05-13 01:50:16 +0500

    Added resolutions choice feature, bug fixes & minor improvements

--- Assets/scripts/Resolutions.cs
+++ Assets/scripts/Resolutions.cs
@@ -5,6 +5,22 @@ using UnityEngine.UI;
 
 public class Resolutions : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Resolution[] rsl;
+    List<string> resolutions;
+    public Dropdown dropdown;
+    public void Awake()
+    {
+        resolutions = new List<string>();
+        rsl = Screen.resolutions;
+        dropdown.ClearOptions();
+        foreach (var res in rsl)
+        {
+            resolutions.Add(res.width + "x" + res.height);
+        }
+        dropdown.AddOptions(resolutions);
+    }
     
+    
+    public void ChangeResolution(int newRes) 
+        => Screen.SetResolution(rsl[newRes].width, rsl[newRes].height, Screen.fullScreen);
 }
