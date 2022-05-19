08a135ac7ef3f562f974c3cf048c1058faa360fd
perceptr <st.arms.all@gmail.com>
2022-05-20 02:20:30 +0500

    Vital Improvements & Bug Fixes

--- Assets/scripts/LanguageChanger.cs
+++ Assets/scripts/LanguageChanger.cs
@@ -4,14 +4,13 @@ public class LanguageChanger : MonoBehaviour
 {
     public void Set_RU()
     {
-        // сохраняем пару ключ-значение
         PlayerPrefs.SetString("GameLanguage", "RU");
-        // выведем сведетельство того, что игра увидела смену языка
-        Debug.Log("Language changed to RUSSIAN");
+        Debug.Log("Language changed to Russian");
     }
+    
     public void Set_EN()
     {
         PlayerPrefs.SetString("GameLanguage", "EN");
-        Debug.Log("Language changed to ENGLISH");
+        Debug.Log("Language changed to English");
     }
 }
--- Assets/scripts/Localizator.cs
+++ Assets/scripts/Localizator.cs
@@ -1,22 +1,20 @@
 using UnityEngine;
-using UnityEngine.UI;
 using System.Text.RegularExpressions;
 using TMPro;
 
 public class Localizator : MonoBehaviour
 {
     public string id;
+    
     public void Update()
-    {  
-        // если язык выбран...
+    {
         if (PlayerPrefs.HasKey("GameLanguage"))
         {
-            var GameLanguage = PlayerPrefs.GetString("GameLanguage");  //  RU/EN
-            ChangeText(GetBetterLocalizedText(id, GameLanguage));
+            var gameLanguage = PlayerPrefs.GetString("GameLanguage");
+            ChangeText(GetBetterLocalizedText(id, gameLanguage));
         }
         else
-        {  
-            // если язык не выбран то английский по умолчанию
+        {
             ChangeText(GetBetterLocalizedText(id, "EN"));
         }
     }
@@ -24,28 +22,28 @@ public class Localizator : MonoBehaviour
     private void ChangeText(string newText) => GetComponent<TMP_Text>().text = newText;
     
   
-    private string GetBetterLocalizedText(string id, string lang)
-    {  
-        // вытаскиваем из таблицы значение
-        // читаем из Resources/Localization/Localization.csv
-        TextAsset mytxtData=(TextAsset)Resources.Load("localization");
-        string loc_txt=mytxtData.text;
-        string[] rows = loc_txt.Split('\n');
-        for (int i = 1; i < rows.Length; i++)
+    private static string GetBetterLocalizedText(string id, string lang)
+    {
+        var myTxtData = (TextAsset)Resources.Load("localization");
+        var rows = myTxtData.text.Split('\n');
+        
+        for (var i = 1; i < rows.Length; i++)
         {
-            string[] cuted_row = Regex.Split(rows[i], ";");
-            if(id == cuted_row[0])
+            var splitRow = Regex.Split(rows[i], ";");
+            
+            if (id == splitRow[0])
             {
-                if (lang == "EN")
+                switch (lang)
                 {
-                    return cuted_row[1];
+                    case "EN":
+                        return splitRow[1];
+                    case "RU":
+                        return splitRow[2];
                 }
-                if(lang == "RU"){
-                    return cuted_row[2];
-                }
+
                 break;
             }
         }
-        return "translation not found";  // если перевод не найден в таблице
+        return "Translation Not Found";
     }
 }
